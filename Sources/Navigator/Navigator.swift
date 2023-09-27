d0c4ed1e0c0bc0fd99894aa0450e9bf020c230c9
Alper Ozturk <alperozturk@lions-macbook.local>
2023-09-27 10:05:40 +0200

    Add Example usages

--- Sources/Navigator/Navigator.swift
+++ Sources/Navigator/Navigator.swift
@@ -1,9 +1,14 @@
 import SwiftUI
 
+/// Navigator is a wrapper that uses UINavigationController.
+///
+/// Navigator uses UIApplication extensions to perform navigation according to needs such as pop, push, popTo specific View, present View modally etc.
 public struct Navigator {
- 
+    
     /// Removes the last element from the navigation stack.
     /// e.g Before: A -> B | After: A
+    ///
+    /// Usage ``Navigator.pop()``
     public static func pop() {
         assert(Thread.isMainThread, "Must be run on main thread")
         
@@ -12,6 +17,8 @@ public struct Navigator {
     
     /// Goes back to first element in the navigation stack.
     /// e.g Before: A -> B -> C -> D | After: A
+    ///
+    /// Usage ``Navigator.popToRoot()`` or ``Navigator.popToRoot(animated: false)``
     public static func popToRoot(animated: Bool = true) {
         assert(Thread.isMainThread, "Must be run on main thread")
         
@@ -25,6 +32,8 @@ public struct Navigator {
     /// Goes back to specific element in the navigation stack with given title of View.
     /// e.g Before: A (title: "Home") -> B (title: "Item List") -> C (title: "Item Detail Page") -> D (title: "User Page")
     /// After: popTo(Item List) A -> B
+    ///
+    /// Usage ``Navigator.popTo("MovieList")``
     @discardableResult
     public static func popTo(_ navigationBarTitle: String, animation: NavigationAnimation? = nil) -> Bool {
         assert(Thread.isMainThread, "Must be run on main thread")
@@ -34,6 +43,8 @@ public struct Navigator {
     
     /// Add new element in the navigation stack. For first use NavigationView must be used.
     /// e.g Before: A -> B -> C | After: A -> B -> C -> D
+    ///
+    /// Usage ``Navigator.push(MovieListView())``
     public static func push<V: View>(_ view: V, animation: NavigationAnimation? = nil, title: String? = nil) {
         assert(Thread.isMainThread, "Must be run on main thread")
         
@@ -41,6 +52,8 @@ public struct Navigator {
     }
     
     /// Remove all elements from navigation stack.
+    ///
+    /// Usage ``Navigator.removeAll()``
     public static func removeAll() {
         assert(Thread.isMainThread, "Must be run on main thread")
         
@@ -50,6 +63,8 @@ public struct Navigator {
     /// Replace navigation stack with given navigation stack.
     /// e.g Before: A -> B -> C
     /// After: D -> E -> F
+    ///
+    /// Usage ``Navigator.setStack([MovieListView(), MovieListDetailView()])``
     public static func setStack<V: View>(_ views: [V]) {
         assert(Thread.isMainThread, "Must be run on main thread")
         
@@ -59,6 +74,8 @@ public struct Navigator {
     /// Replace the navigation stack with a single View. You can achieve the same result by using setStack with a single element inside it.
     /// e.g Before: A -> B -> C
     /// After: D
+    ///
+    /// Usage ``Navigator.setRoot(HomeView())``
     public static func setRoot<V: View>(_ view: V) {
         assert(Thread.isMainThread, "Must be run on main thread")
         
@@ -66,6 +83,8 @@ public struct Navigator {
     }
     
     /// Present given View modally.
+    ///
+    /// Usage ``Navigator.presentModal(AddMovieBottomSheet())``
     public static func presentModal<V: View>(_ view: V, title: String? = nil) {
         assert(Thread.isMainThread, "Must be run on main thread")
         
